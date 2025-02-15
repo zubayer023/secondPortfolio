94317836a5ca77965825bcfd93872ea098701cfe
zubayer <zubayer.org@gmail.com>
2025-02-15 11:30:34 +0600

    title chang

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,27 +1,22 @@
-
-import ProjectCard from './ProjectCard';
-import { projects } from './../data';
-import SectionTitle from './SectionTitle';
-
-
+import ProjectCard from "./ProjectCard";
+import { projects } from "./../data";
+import SectionTitle from "./SectionTitle";
 
 const Projects = () => {
   return (
-    <section className='my-element py-20' id='projects'>
-        <SectionTitle text='web creations' />
-        <div className='py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8'>
-            {
-                projects.map((project)=>{
-                    return(
-                        <div key={project.id}>
-                           <ProjectCard {...project} />
-                        </div>
-                    )
-                })
-            }
-        </div>
+    <section className="my-element py-20" id="projects">
+      <SectionTitle text="web creations" />
+      <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
+        {projects.map((project) => {
+          return (
+            <div key={project.id}>
+              <ProjectCard {...project} />
+            </div>
+          );
+        })}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
